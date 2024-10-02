4ef1c9fee34baea4ab4fce7552b012b21b600cbf
parham-devdocs <p.77.pazargadi@gmail.com>
2024-10-02 23:33:05 +0330

    code improved

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import tailwindcss from "tailwindcss";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/vite-react-deploy",
   css: {
     postcss: {
     plugins:[tailwindcss()]
